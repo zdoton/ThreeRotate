1a97cc134e34cd3d3399ce0b953e2a6fca711093
zdoton <39454106+zdoton@users.noreply.github.com>
2022-02-28 14:12:54 +0900

    first

---
+++ imgrot.js
@@ -0,0 +1,84 @@
+// シーン
+var scene = new THREE.Scene();
+// レンダラー
+var renderer = new THREE.WebGLRenderer({alpha:true, antialias: true });
+renderer.setClearColor(0x0000ff,1);
+renderer.setSize( window.innerWidth, window.innerHeight );
+//renderer.setPixelRatio(window.devicePixelRatio ? window.devicePixelRatio : 1);
+document.body.appendChild( renderer.domElement );
+// カメラ
+var camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 1, 1500 );
+camera.position.set(0, 0, 1000);
+const controls = new THREE.OrbitControls(camera, document.body);
+// 初期化のために実行
+onResize();
+// リサイズイベント発生時に実行
+window.addEventListener('resize', onResize);
+// ライト
+var light = new THREE.AmbientLight( 0xffffff );
+scene.add( light );
+
+// Points生成
+var pointsGeometry;
+var pointsMaterial = new THREE.PointsMaterial({
+  color: 0xFF00FF,
+  size: 5,
+});
+
+//無理やり読み込み
+var cv = document.createElement('canvas');
+var ctx = cv.getContext('2d');
+var pixelData;
+var imageWidth; var imageHeight;
+const points = [];
+const indicies = [];
+const uvs = [];
+const image = new Image();
+image.src = '5000.png';
+
+image.onload = () => {
+    imageWidth = image.width; imageHeight = image.height;
+    cv.width = imageWidth;
+    cv.height = imageHeight;
+    ctx.drawImage(image, 0, 0);
+    var pixelData = ctx.getImageData(0, 0, imageWidth, imageHeight).data;
+    console.log(pixelData);
+    console.log(pixelData.length);
+
+    for(var x = 0; x < imageWidth; x++) {
+        for(var y = 0; y < imageHeight; y++) {
+            if (pixelData[(x + y * imageWidth) * 4 + 3] === 0) {
+                continue;
+            }
+            points.push(new THREE.Vector3(x, -y, 1));
+        }
+    }
+
+    pointsGeometry = new THREE.BufferGeometry().setFromPoints(points);
+
+    const pointsMesh = new THREE.Points(pointsGeometry, pointsMaterial);
+    scene.add(pointsMesh);
+}
+
+
+// レンダリング
+function render() {
+    requestAnimationFrame(render);
+    renderer.render(scene, camera);
+}
+render();
+
+
+function onResize() {
+    // サイズを取得
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+  
+    // レンダラーのサイズを調整する
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(width, height);
+  
+    // カメラのアスペクト比を正す
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+}
